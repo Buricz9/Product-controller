fc828fc4dea97af105c69589d073373fe0d48216
buricz9 <buricz1906@gmail.com>
2025-03-05 20:50:43 +0100

    basic connection + first migration

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	dsn := "admin:admin@tcp(localhost:3306)/productdb"
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Błąd połączenia:", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal("Błąd pingowania bazy:", err)
+	}
+
+	fmt.Println("Połączono z bazą danych MariaDB!")
 }
